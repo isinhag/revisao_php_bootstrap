54c5bc17ac69dbaf4ac3eb916397bef99687a4a9
isinhag <130663230+isinhag@users.noreply.github.com>
2024-02-08 16:32:18 -0300

    atualizacoes
    código de apagar não finalizado

--- apagar.php
+++ apagar.php
@@ -1,2 +1,9 @@
-<!-- Digite aqui (apagar.php) -->
-<!-- 5º Arquivo a ser digitado -->
+<?php
+
+// Incluir a conexao com o banco de dados
+include_once "conexao.php";
+
+// Receber os dados do formulário via método POST
+$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
+
+
