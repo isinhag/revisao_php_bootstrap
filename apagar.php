79bb17c75f362a0e245fd3e5a40403b5e668c7b6
isinhag <130663230+isinhag@users.noreply.github.com>
2024-02-09 10:37:04 -0300

    o

--- apagar.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Incluir a conexao com o banco de dados
-include_once "conexao.php";
-
-// Receber os dados do formulário via método POST
-$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
-
-
